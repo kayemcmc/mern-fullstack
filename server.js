fbe37c195de384d80b5e0c090c321760961bf181
Karen Posada <k100posada@gmail.com>
2018-04-21 15:34:55 -0500

    setting up express server routes

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+//Use routes
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
 
 app.get('/', (req, res) => res.send('Hello KP'));
 
